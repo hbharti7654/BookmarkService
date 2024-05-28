944717c0f421c9951d2cfa32ccdca48e166bd9c0
Harsh Bharti <hbharti7654@gmail.com>
2024-05-28 12:57:34 +0530

    Pushing repo

---
+++ src/main/java/com/flexifurns/bookmark/BookmarkApplication.java
@@ -0,0 +1,14 @@
+package com.flexifurns.bookmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Its Started");
+		SpringApplication.run(BookmarkApplication.class, args);
+	}
+
+}
